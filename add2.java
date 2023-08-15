2e55447557cfa1a8da07b10af0fc182afd548309
SasikumarSentthil <sasikumarcodes@gmail.com>
2023-08-15 15:10:09 +0530

    addition

---
+++ add2.java
@@ -0,0 +1,12 @@
+class add2
+{
+    public static void main(String str[])
+    {
+        int a,b,c,d;
+        a=10;
+        b=4;
+        c=2;
+        d=a+b+c;
+        System.out.println(d)
+    }
+}

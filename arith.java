b248d1c63c5f447010681314605ea84f1c19c811
SasikumarSentthil <sasikumarcodes@gmail.com>
2023-08-16 21:08:04 +0530

    basic arithmetic operations

---
+++ arith.java
@@ -0,0 +1,17 @@
+class arith
+{
+    public static void main(String str[])
+    {
+        int a,b,c,d,e,f;
+        a=10;
+        b=5;
+        c=a+b;
+        d=a-b;
+        e=a*b;
+        f=a/b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
